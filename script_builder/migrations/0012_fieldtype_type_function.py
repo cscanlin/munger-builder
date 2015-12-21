51af6ea74a2e08b6004635070acd294000c7d6f3
Christopher Scanlin <cscanlin@gmail.com>
2015-12-21 12:49:22 -0800

    fix munger builder output2

---
+++ script_builder/migrations/0012_fieldtype_type_function.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('script_builder', '0011_auto_20151221_1108'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='fieldtype',
+            name='type_function',
+            field=models.CharField(default='ch', max_length=200),
+            preserve_default=False,
+        ),
+    ]
