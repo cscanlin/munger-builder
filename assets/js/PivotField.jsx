4a5da739a6af2581857c7d717029a57b789f13db
Chris Scanlin <cscanlin@gmail.com>
2016-07-26 21:38:32 -0700

    minor updates to prepare PivotField

--- assets/js/PivotField.jsx
+++ assets/js/PivotField.jsx
@@ -6,10 +6,19 @@ class PivotField extends React.Component {
     super(props);
     this.state = {
       data_field: this.props.data_field,
-      field_type: 3, // Redux
-      active_name: this.props.active_name,
+      field_type: this.props.field_type,
+      active_name: this.props.active_name, // Redux
     };
     this.fieldTypeName = this.fieldTypeName.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('created pivot field');
+  }
+
+  componentWillUnmount() {
+    console.log('removed pivot field');
   }
 
   fieldTypeName() {
@@ -18,13 +27,17 @@ class PivotField extends React.Component {
   }
 
   render() {
-    return (<div>{this.state.active_name} - {this.fieldTypeName()}</div>);
+    return (
+      <div onClick={this.onClick}>
+        {this.state.active_name} - {this.fieldTypeName()}
+      </div>);
   }
 
 }
 
 PivotField.propTypes = {
   data_field: React.PropTypes.number.isRequired,
+  field_type: React.PropTypes.number.isRequired,
   active_name: React.PropTypes.string.isRequired, // Redux
 };
 module.exports = PivotField;
