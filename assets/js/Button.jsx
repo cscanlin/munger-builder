7e6a47a0820a5cfb179796bb0258e38700d228e5
Chris Scanlin <cscanlin@gmail.com>
2016-07-31 10:42:07 -0700

    remove isRequired from most Button props

--- assets/js/Button.jsx
+++ assets/js/Button.jsx
@@ -5,10 +5,10 @@ function Button(props) {
 }
 
 Button.propTypes = {
-  type: React.PropTypes.string.isRequired,
+  type: React.PropTypes.string,
   src: React.PropTypes.string,
   value: React.PropTypes.string.isRequired,
-  className: React.PropTypes.string.isRequired,
+  className: React.PropTypes.string,
   onClick: React.PropTypes.func.isRequired,
 }
 module.exports = Button
