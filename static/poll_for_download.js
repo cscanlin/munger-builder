a3ea9bb2a61cbda25b2747f02224252bbf44b4bb
Chris Scanlin <cscanlin@gmail.com>
2016-08-01 19:56:58 -0700

    delete many old files, poll_for_download not working

--- static/poll_for_download.js
+++ static/poll_for_download.js
@@ -1,22 +1,24 @@
-$(document).ready(function(){
-    $.ajaxSetup({ cache: false, timeout: 360000 });
-    var url = "/script_builder/poll_for_download/";
-    var i = 0;
-    var task_id = $('#task-id').attr('value');
-    (function worker() {
-        $.getJSON(url+"?task_id="+task_id, function(data){
-            console.log(data);
-            if(data.filename) {
-                var file_url = url+"?filename="+data.filename;
-                $("#download").html("If your download doesn't start automatically, please click <a href='"+file_url+"'>here</a>.");
-                window.location.href = file_url;
-            } else {
-                setTimeout(worker, 5000);
-            }
-        });
-    })();
-    setInterval(function() {
-        i = ++i % 4;
-        $("#loading").html("loading"+Array(i+1).join("."));
-    }, 1000);
-});
+$(document).ready(() => {
+  $.ajaxSetup({ cache: false, timeout: 360000 })
+  const url = '/script_builder/poll_for_download/'
+  let i = 0
+  const taskId = $('#task-id').attr('value')
+  (function worker() {
+    $.getJSON(`${url}?task_id=${taskId}`, function(data) {
+      console.log(data)
+      if (data.filename) {
+        const fileUrl = `${url}?filename=${data.filename}`
+        $("#download").html(
+          `<a href='${fileUrl}'>Click here if your download doesn't start automatically.</a>`
+        )
+        window.location.href = fileUrl
+      } else {
+        setTimeout(worker, 5000)
+      }
+    })
+  })()
+  setInterval(() => {
+    i = ++i % 4
+    $("#loading").html("loading"+Array(i+1).join("."))
+  }, 1000)
+})
