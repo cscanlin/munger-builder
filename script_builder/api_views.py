8102cfb2602c0856bb13e88442827633b9e4da48
Chris Scanlin <cscanlin@gmail.com>
2016-08-08 20:10:15 -0700

    permissions hotfix

--- script_builder/api_views.py
+++ script_builder/api_views.py
@@ -9,6 +9,7 @@ class MungerPermissions(permissions.DjangoObjectPermissions):
     perms_map = {
         'GET': ['%(app_label)s.change_%(model_name)s'],
         'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
         'DELETE': ['%(app_label)s.delete_%(model_name)s'],
         'OPTIONS': ['%(app_label)s.change_%(model_name)s'],
         'HEAD': ['%(app_label)s.change_%(model_name)s'],
