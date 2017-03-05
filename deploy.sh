8a8b404149080ae4b649d85d387398c470578a15
Chris Scanlin <cscanlin@gmail.com>
2017-03-04 16:31:43 -0800

    add script comments to reset app in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 git push origin master
 git push heroku master
-heroku run python manage.py loaddata initial_data --app munger-builder
-heroku run python manage.py migrate --app munger-builder
+heroku run python manage.py migrate -a munger-builder
+
+# To fully reset:
+# heroku pg:reset DATABASE -a munger-builder --confirm munger-builder
+# heroku run python manage.py migrate -a munger-builder
+# heroku run python manage.py loaddata initial_data -a munger-builder
+# heroku run python manage.py createsuperuser -a munger-builder
+# (Enter Credentials)
+# heroku restart -a app_name
